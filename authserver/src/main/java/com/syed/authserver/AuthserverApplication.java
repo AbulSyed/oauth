0de69bd638cd5d04900cc58dc588340cdaa0eca5
Abul Syed <abulsyed@Abuls-MacBook-Air.local>
2022-12-20 20:35:00 +0000

    auth server

---
+++ authserver/src/main/java/com/syed/authserver/AuthserverApplication.java
@@ -0,0 +1,13 @@
+package com.syed.authserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthserverApplication.class, args);
+	}
+
+}
---
+++ authserver/src/test/java/com/syed/authserver/AuthserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.syed.authserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
